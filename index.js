0828d6c78bbb7137b6334dd6c381900700535c4d
bsa-git <bsa2657@yandex.ru>
2018-06-14 10:19:21 +0300

    Update Project

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var cool = require('cool-ascii-faces');
 var express = require('express');
 var app = express();
 
-app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 3000));
 
 app.use(express.static(__dirname + '/public'));
 
@@ -11,36 +11,39 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
-  response.render('pages/index')
+app.get('/', function (request, response) {
+    response.render('pages/index')
 });
 
-app.get('/cool', function(request, response) {
-  response.send(cool());
+app.get('/cool', function (request, response) {
+    response.send(cool());
 });
 
-app.get('/times', function(request, response) {
+app.get('/times', function (request, response) {
     var result = ''
     var times = process.env.TIMES || 5
-    for (i=0; i < times; i++)
-      result += i + ' ';
-  response.send(result);
+    for (i = 0; i < times; i++)
+        result += i + ' ';
+    response.send(result);
 });
 
 app.get('/db', function (request, response) {
-  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * FROM test_table', function(err, result) {
-      done();
-      if (err)
-       { console.error(err); response.send("Error " + err); }
-      else
-       { response.render('pages/db', {results: result.rows} ); }
+    pg.connect(process.env.DATABASE_URL, function (err, client, done) {
+        client.query('SELECT * FROM test_table', function (err, result) {
+            done();
+            if (err) {
+                console.error(err);
+                response.send("Error " + err);
+            }
+            else {
+                response.render('pages/db', {results: result.rows});
+            }
+        });
     });
-  });
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+app.listen(app.get('port'), function () {
+    console.log('Node app is running on port', app.get('port'));
 });
 
 
